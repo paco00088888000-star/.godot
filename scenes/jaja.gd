e30346545b8d99f2ce0a7c091ae88a1574614bb9
Patrik <paco00088888000@gmail.com>
2026-04-03 22:34:03 +0200

    glitch

--- scenes/jaja.gd
+++ scenes/jaja.gd
@@ -3,47 +3,48 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
-# Referenca na AnimatedSprite2D čvor
+# Referenca na tvoj AnimatedSprite2D čvor
 @onready var sprite = $AnimatedSprite2D
 
-# Varijabla koja prati napada li igrač trenutno
 var is_attacking = false
 
 func _physics_process(delta: float) -> void:
-	# 1. Dodavanje gravitacije
+	# 1. Gravitacija
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# 2. Napad (Attack)
-	# Koristimo "ui_focus_next" kao primjer (Tab), zamijeni s "attack" u Input Map-u
-	if Input.is_action_just_pressed("ui_focus_next") and not is_attacking:
-		attack()
+	# 2. Napad (LMB klik)
+	# Napomena: Moraš dodati "attack" u Project Settings -> Input Map
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		perform_attack()
 
-	# 3. Skok (Jump)
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 3. Skok
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not is_attacking:
 		velocity.y = JUMP_VELOCITY
 
-	# 4. Kretanje (Walk)
+	# 4. Kretanje (Lijevo/Desno)
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
-	# Okretanje sprite-a ovisno o smjeru
-	if direction > 0:
-		sprite.flip_h = false
-	elif direction < 0:
-		sprite.flip_h = true
+	# Okretanje lika (ako ne napadaš, dopusti okretanje)
+	if not is_attacking:
+		if direction > 0:
+			sprite.flip_h = false
+		elif direction < 0:
+			sprite.flip_h = true
 
-	if direction:
+	# Kretanje se usporava ako napadamo (opcionalno, ovisi o tvojoj igri)
+	if direction and not is_attacking:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
-	# 5. Ažuriranje animacija
+	# 5. Upravljanje animacijama
 	update_animations(direction)
 
 func update_animations(direction):
-	# Ako napadamo, ne mijenjaj animaciju dok ne završi
+	# Ako je napad u tijeku, ne mijenjaj animaciju
 	if is_attacking:
 		return
 		
@@ -54,10 +55,10 @@ func update_animations(direction):
 	else:
 		sprite.play("idle")
 
-func attack():
+func perform_attack():
 	is_attacking = true
 	sprite.play("attack")
 	
-	# Čekamo da animacija završi prije nego dopustimo kretanje/drugu animaciju
+	# Čekamo da animacija završi
 	await sprite.animation_finished
 	is_attacking = false
